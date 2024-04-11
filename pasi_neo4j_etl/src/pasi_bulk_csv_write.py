b8729c4e220edf6c3fa6b6e3f94133e13190da93
CORP\palmaro <oscar.palmar@powerschool.com>
2024-04-11 17:55:30 -0400

    more updates to raw data

--- pasi_neo4j_etl/src/pasi_bulk_csv_write.py
+++ pasi_neo4j_etl/src/pasi_bulk_csv_write.py
@@ -45,30 +45,7 @@ def load_pasi_graph_from_csv() -> None:
         for node in NODES:
             session.execute_write(_set_uniqueness_constraints, node)
 
-    LOGGER.info("Loading student nodes")
-    with driver.session(database="neo4j") as session:
-        query = f"""
-        LOAD CSV WITH HEADERS
-        FROM '{STUDENTS_CSV_PATH}' AS students with students where students.STUDENT_GENDER is not null
-        MERGE (s:Student {{id: toInteger(students.STUDENT_ASN),
-                            last_name: students.STUDENT_LAST_NAME,
-                            first_name: students.STUDENT_FIRST_NAME,
-                            dob: students.STUDENT_BIRTH_DATE,
-                            gender: students.STUDENT_GENDER,
-                            citizenship_status: students.STUDENT_CITIZENSHIP_STATUS
-        }})
-            ON CREATE SET s.language_at_home = students.STUDENT_LANGUAGE_AT_HOME
-            ON MATCH SET s.language_at_home = students.STUDENT_LANGUAGE_AT_HOME
-            ON CREATE SET s.phone_number = students.STUDENT_PHONE_NUMBER
-            ON MATCH SET s.phone_number = students.STUDENT_PHONE_NUMBER
-            ON CREATE SET s.phone_start_date = students.STUDENT_PHONE_START_DATE
-            ON MATCH SET s.phone_start_date = students.STUDENT_PHONE_START_DATE
-            ON CREATE SET s.phone_end_date = students.STUDENT_PHONE_END_DATE
-            ON MATCH SET s.phone_end_date = students.STUDENT_PHONE_END_DATE
-            ON CREATE SET s.country_of_citizenship = students.STUDENT_COUNTRY_OF_CITIZENSHIP
-            ON MATCH SET s.country_of_citizenship = students.STUDENT_COUNTRY_OF_CITIZENSHIP                                                
-        """
-        _ = session.run(query, {})
+
 
     LOGGER.info("Loading school nodes")
     with driver.session(database="neo4j") as session:
